4e91828250c82dfaf7524870d6584aae8b90ec30
Adi Turaga <adi.s.turaga@gmail.com>
2025-01-16 12:38:50 -0500

    Changed from snake case to camel case

--- src/Tabs.jsx
+++ src/Tabs.jsx
@@ -3,15 +3,15 @@ import axios from 'axios';
 
 function Tabs() {
 
-    const text_btn = useRef(null);
-    const img_btn = useRef(null);
-    const qr_btn = useRef(null);
-    const btns = [text_btn, img_btn, qr_btn];
+    const textBtn = useRef(null);
+    const imgBtn = useRef(null);
+    const qrBtn = useRef(null);
+    const btns = [textBtn, imgBtn, qrBtn];
 
-    const text_div = useRef(null);
-    const img_div = useRef(null);
-    const qr_div = useRef(null);
-    const divs = [text_div, img_div, qr_div]
+    const textDiv = useRef(null);
+    const imgDiv = useRef(null);
+    const qrDiv = useRef(null);
+    const divs = [textDiv, imgDiv, qrDiv]
 
     const [color, setColor] = useState("");
     const [text, setText] = useState("");
@@ -19,18 +19,18 @@ function Tabs() {
     const [imgDisp, setImgDisp] = useState("");
 
     useEffect(() => {
-        text_btn.current.style.border = "3px solid #ffa07a";
-        img_btn.current.style.border = "3px solid #00bfff";
-        qr_btn.current.style.border = "3px solid #26d4b7";
+        textBtn.current.style.border = "3px solid #ffa07a";
+        imgBtn.current.style.border = "3px solid #00bfff";
+        qrBtn.current.style.border = "3px solid #26d4b7";
     }, [])
 
-    function change_text(event) { setText(event.target.value); }
-    function show_image(event) {
+    function changeText(event) { setText(event.target.value); }
+    function showImage(event) {
         setImg(event.target.files[0]);
         setImgDisp(URL.createObjectURL(event.target.files[0]));
     }
 
-    function show_content(btn, div, color) {
+    function showContent(btn, div, color) {
         for(const _div of divs) { _div.current.style.display = 'none'; }
         for(const _btn of btns) { _btn.current.style.backgroundColor = ''; }
         div.current.style.display = 'block';
@@ -38,7 +38,7 @@ function Tabs() {
         setColor(color);
     }
 
-    function submit_text_qr(type) {
+    function submitTextQr(type) {
         console.log(text);
         const config = {
             "headers": { "Content-Type": "application/json" }
@@ -53,7 +53,7 @@ function Tabs() {
         setText("");
     }
 
-    const submit_img = (e) => {
+    const submitImg = (e) => {
         e.preventDefault();
         console.log(img);
         const config = {
@@ -73,25 +73,25 @@ function Tabs() {
         <h1>Print your Receipt!</h1>
         <hr />
         <div className="btns-panel">
-            <button ref={text_btn} onClick={() => show_content(text_btn, text_div, '#ffa07a')}><img src="../src/assets/text-tool.svg"></img></button>
-            <button ref={img_btn} onClick={() => show_content(img_btn, img_div, '#00bfff')}><img src="../src/assets/image-1.svg"></img></button>
-            <button ref={qr_btn} onClick={() => show_content(qr_btn, qr_div, '#26d4b7')}><img src="../src/assets/qr-code-scan.svg"></img></button>
+            <button ref={textBtn} onClick={() => showContent(textBtn, textDiv, '#ffa07a')}><img src="../src/assets/text-tool.svg"></img></button>
+            <button ref={imgBtn} onClick={() => showContent(imgBtn, imgDiv, '#00bfff')}><img src="../src/assets/image-1.svg"></img></button>
+            <button ref={qrBtn} onClick={() => showContent(qrBtn, qrDiv, '#26d4b7')}><img src="../src/assets/qr-code-scan.svg"></img></button>
         </div>
 
-        <div ref={text_div} className="tab-content" id="text-content">
-            <textarea onChange={change_text} placeholder="Type some text..."/><br/>
-            <button className="submit" onClick={() => submit_text_qr('text')} style={{backgroundColor: color}}>Submit</button>
+        <div ref={textDiv} className="tab-content" id="text-content">
+            <textarea onChange={changeText} placeholder="Type some text..."/><br/>
+            <button className="submit" onClick={() => submitTextQr('text')} style={{backgroundColor: color}}>Submit</button>
         </div>
 
-        <div ref={img_div} className="tab-content" id="img-content">
-            <input type="file" accept="image/*" onChange={show_image}/><br/>
+        <div ref={imgDiv} className="tab-content" id="img-content">
+            <input type="file" accept="image/*" onChange={showImage}/><br/>
             <img src={imgDisp}/>
-            <button className="submit" onClick={submit_img} style={{backgroundColor: color}}>Submit</button>
+            <button className="submit" onClick={submitImg} style={{backgroundColor: color}}>Submit</button>
         </div>
 
-        <div ref={qr_div} className="tab-content" id="qr-content">
-            <textarea onChange={change_text} placeholder="Enter a link or type some text..."/><br/>
-            <button className="submit" onClick={() => submit_text_qr('qr')} style={{backgroundColor: color}}>Submit</button>
+        <div ref={qrDiv} className="tab-content" id="qr-content">
+            <textarea onChange={changeText} placeholder="Enter a link or type some text..."/><br/>
+            <button className="submit" onClick={() => submitTextQr('qr')} style={{backgroundColor: color}}>Submit</button>
         </div>
     </>
     )
